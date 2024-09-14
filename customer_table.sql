6a2b162471bb926fd2ed58a8c1b44b34e808e257
Anant Kanchan <anant4265@gmail.com>
2024-09-14 18:05:47 +0530

    init

---
+++ customer_table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Customer_table(
+Cust_id int,
+First_name varchar,
+Last_name varchar,
+age int,
+email_id varchar);
+
+INSERT INTO Customer_table
+VALUES (1,'bee','cee',32,'bc@xyz.com')
+
+INSERT INTO Customer_table(cust_id, first_name,age,email_id)
+VALUES (2,'dee',23,'d@xyz.com')
+
+INSERT INTO Customer_table VALUES
+(3,'ee','ef',27,'ef@xyz.com'),
+(4,'gee','eh',35,'gh@xyz.com')
+
+copy Customer_table from '/Users/postgres (Deleted)/copy.csv' DELIMITER ',' CSV HEADER;
+
+copy Customer_table from '/Users/postgres (Deleted)/copytext.txt' DELIMITER ',';
