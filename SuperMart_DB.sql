a4a6ab884e6d8b59bc233ff9092b47f8bc65108f
Anant Kanchan <anant4265@gmail.com>
2024-09-23 14:35:10 +0530

    left join

--- SuperMart_DB.sql
+++ SuperMart_DB.sql
@@ -150,3 +150,20 @@ FROM sales_2015 AS a
 INNER JOIN customer_20_60 AS b
 ON a.customer_id = b.customer_id
 ORDER BY customer_id;
+
+/* LEFT JOIN */
+
+SELECT customer_id FROM sales_2015 ORDER BY customer_id;
+SELECT customer_id FROM customer_20_60 ORDER BY customer_id;
+
+SELECT
+	a.order_line,
+	a.product_id,
+	a.customer_id,
+	a.sales,
+	b.customer_name,
+	b.age
+FROM sales_2015 AS a
+LEFT JOIN customer_20_60 AS b
+ON a.customer_id = b.customer_id
+ORDER BY customer_id;
