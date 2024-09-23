d8c181a98c6d25946493f744a26fbc70df097be0
Anant Kanchan <anant4265@gmail.com>
2024-09-23 16:22:29 +0530

    union

--- SuperMart_DB.sql
+++ SuperMart_DB.sql
@@ -233,3 +233,10 @@ SELECT customer_id FROM sales_2015
 EXCEPT
 SELECT customer_id FROM customer_20_60
 ORDER BY customer_id;
+
+/* UNION */
+
+SELECT customer_id FROM sales_2015
+UNION
+SELECT customer_id FROM customer_20_60
+ORDER BY customer_id;
