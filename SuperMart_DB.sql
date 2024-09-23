d05fd3be5617f05204c026704ae43c9dfdbd6a41
Anant Kanchan <anant4265@gmail.com>
2024-09-23 16:17:06 +0530

    except

--- SuperMart_DB.sql
+++ SuperMart_DB.sql
@@ -224,3 +224,12 @@ SELECT customer_id FROM sales_2015
 INTERSECT
 SELECT customer_id FROM customer_20_60
 
+/* EXCEPT */
+
+SELECT customer_id FROM sales_2015 ORDER BY customer_id;
+SELECT customer_id FROM customer_20_60 ORDER BY customer_id;
+
+SELECT customer_id FROM sales_2015
+EXCEPT
+SELECT customer_id FROM customer_20_60
+ORDER BY customer_id;
