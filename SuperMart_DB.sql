6687792af0d879b98ac7758e65fd4bbe18875bd4
Anant Kanchan <anant4265@gmail.com>
2024-09-23 14:47:25 +0530

    right join

--- SuperMart_DB.sql
+++ SuperMart_DB.sql
@@ -167,3 +167,20 @@ FROM sales_2015 AS a
 LEFT JOIN customer_20_60 AS b
 ON a.customer_id = b.customer_id
 ORDER BY customer_id;
+
+/* RIGHT JOIN */
+
+SELECT customer_id FROM sales_2015 ORDER BY customer_id;
+SELECT customer_id FROM customer_20_60 ORDER BY customer_id;
+
+SELECT
+	a.order_line,
+	a.product_id,
+	a.customer_id,
+	a.sales,
+	b.customer_name,
+	b.age
+FROM sales_2015 AS a
+RIGHT JOIN customer_20_60 AS b
+ON a.customer_id = b.customer_id
+ORDER BY customer_id;
