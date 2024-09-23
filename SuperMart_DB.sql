343b02a69e6f1a5508d22d60efa51e00826c54d8
Anant Kanchan <anant4265@gmail.com>
2024-09-23 16:50:29 +0530

    subquery

--- SuperMart_DB.sql
+++ SuperMart_DB.sql
@@ -240,3 +240,18 @@ SELECT customer_id FROM sales_2015
 UNION
 SELECT customer_id FROM customer_20_60
 ORDER BY customer_id;
+
+/* SUBQUERY */
+
+SELECT * FROM sales
+WHERE customer_id IN (SELECT customer_id FROM customer WHERE age > 60);
+
+SELECT a.product_id, a.product_name, a.category,b.quantity
+FROM product AS a
+LEFT JOIN (SELECT product_id,SUM(quantity) AS quantity FROM sales GROUP BY product_id) AS b
+ON a.product_id=b.product_id
+ORDER BY b.quantity DESC;
+
+SELECT customer_id, order_line, (SELECT customer_name FROM customer WHERE customer .customer_id=sales.customer_id)
+FROM sales
+ORDER BY customer_id;
